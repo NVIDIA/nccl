d25b1311e1388462f69470fd40f0c66e16b52ee0
Sylvain Jeaugey <sjeaugey@nvidia.com>
2016-11-17 10:16:06 -0800

    Restore cudaMemcpy -- works on gfortran 4.9.2

--- fortran/src/cudafor.f90
+++ fortran/src/cudafor.f90
@@ -82,17 +82,33 @@ end function cudaMalloc
 end interface cudaMalloc
 !End cudaMalloc
 
-!Start cudaMemcpy
-interface cudaMemcpy
-integer(c_int) function cudaMemcpy(dst, src, count, kind) bind(c, name = "cudaMemcpy")
+!Start cudaMemCpy
+interface cudaMemCpy
+
+!Start cudaMemCpyH2D
+integer(c_int) function cudaMemCpyH2D(dst, src, count, kind) bind(c, name = "cudaMemcpy")
 import :: c_ptr, c_int, c_size_t
-import :: c_devptr, cudaMemcpyKind
+import :: c_devptr, cudaMemCpyKind
 implicit none
 type(c_devptr), value :: dst
 type(c_ptr), value :: src
 integer(c_size_t), value :: count
-type(cudaMemcpyKind), value :: kind
-end function cudaMemcpy
+type(cudaMemCpyKind), value :: kind
+end function cudaMemCpyH2D
+!End cudaMemCpyH2D
+
+!Start cudaMemCpyD2H
+integer(c_int) function cudaMemCpyD2H(dst, src, count, kind) bind(c, name = "cudaMemcpy")
+import :: c_ptr, c_int, c_size_t
+import :: c_devptr, cudaMemCpyKind
+implicit none
+type(c_ptr), value :: dst
+type(c_devptr), value :: src
+integer(c_size_t), value :: count
+type(cudaMemCpyKind), value :: kind
+end function cudaMemCpyD2H
+!End cudaMemCpyD2H
+
 end interface cudaMemcpy
 !End cudaMemcpy
 

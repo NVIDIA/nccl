976e8f5bab15f6eff961f7f45628bee36047d7bc
Soojeong <epik03sj@gmail.com>
2019-05-09 13:53:28 +0900

    fix bug

--- src/include/stat_collector.h
+++ src/include/stat_collector.h
@@ -18,14 +18,14 @@
 inline void create_comm_stat(ncclProf_t* nccl_prof, commType_t comm_type,
     int from_rank, int to_rank, uint64_t start_micros, uint64_t end_micros, int comm_bytes) {
   pthread_mutex_lock(&nccl_prof->mu_);
-  commStat_t comm_stat;
-  comm_stat.comm_type = comm_type;
-  comm_stat.from_rank = from_rank;
-  comm_stat.to_rank = to_rank;
-  comm_stat.start_micros = start_micros;
-  comm_stat.end_micros = end_micros;
-  comm_stat.comm_bytes = comm_bytes;
-  nccl_prof->stat_vector.push_back(comm_stat);
+  commStat_t* comm_stat = (commStat_t*) malloc(sizeof(commStat_t));
+  comm_stat->comm_type = comm_type;
+  comm_stat->from_rank = from_rank;
+  comm_stat->to_rank = to_rank;
+  comm_stat->start_micros = start_micros;
+  comm_stat->end_micros = end_micros;
+  comm_stat->comm_bytes = comm_bytes;
+  nccl_prof->stat_vector->push_back(comm_stat);
   pthread_mutex_unlock(&nccl_prof->mu_);
 }
 

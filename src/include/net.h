5328685195ecd8b7a90c9dadf3c1003f4500a0ee
Danilo <doak@google.com>
2020-04-16 23:18:00 +0200

    Extend network plugin to support allocations.
    Implement Alloc/Free functions to give more control over the memory
    being allocated to the memory plugins.
    
    These new functions are unsed instead of the memory registration
    functions, so I decided to make these changes backwards-compatible by
    requiring that plugins using the alloc/free calls set the reg/unreg
    field to NULL.
    
    The memory returned by alloc/free have some requirements, which are
    documented in nccl_net.h.

--- src/include/net.h
+++ src/include/net.h
@@ -21,8 +21,8 @@ static ncclResult_t ncclNetListen(int dev, void* handle, void** listenComm) { NC
 static ncclResult_t ncclNetConnect(int dev, void* handle, void** sendComm) { NCCLCHECK(ncclNet->connect(dev, handle, sendComm)); return ncclSuccess; }
 static ncclResult_t ncclNetAccept(void* listenComm, void** recvComm) { NCCLCHECK(ncclNet->accept(listenComm, recvComm)); return ncclSuccess; }
 static bool ncclUsePluginAlloc() { return (void*) ncclNet->regMr == nullptr; }
-static ncclResult_t ncclNetAlloc(void* comm, void** data, int size, int type, void** mhandle) { NCCLCHECK(ncclNet->regMr(comm, data, size, type, mhandle)); return ncclSuccess; }
-static ncclResult_t ncclNetFree(void* comm, void* mhandle) { NCCLCHECK(ncclNet->deregMr(comm, mhandle)); return ncclSuccess; }
+static ncclResult_t ncclNetAlloc(void* comm, void** data, int size, int type, void** mhandle) { NCCLCHECK(ncclNet->alloc(comm, size, type, data, mhandle)); return ncclSuccess; }
+static ncclResult_t ncclNetFree(void* comm, void* mhandle) { NCCLCHECK(ncclNet->free(comm, mhandle)); return ncclSuccess; }
 static ncclResult_t ncclNetRegMr(void* comm, void* data, int size, int type, void** mhandle) { NCCLCHECK(ncclNet->regMr(comm, data, size, type, mhandle)); return ncclSuccess; }
 static ncclResult_t ncclNetDeregMr(void* comm, void* mhandle) { NCCLCHECK(ncclNet->deregMr(comm, mhandle)); return ncclSuccess; }
 static ncclResult_t ncclNetIsend(void* sendComm, void* data, int size, void* mhandle, void** request) { NCCLCHECK(ncclNet->isend(sendComm, data, size, mhandle, request)); return ncclSuccess; }

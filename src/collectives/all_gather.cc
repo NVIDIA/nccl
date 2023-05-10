abc65b63fa53fc210d86d859d889511891ad6105
Greg Steinbrecher <grs@meta.com>
2023-05-10 13:31:55 -0700

    Use a2a-based all-gather when message size is <= 512kiB
    Summary:
    For non-local (no nvlink-local GPUs) all-gather operations, we see that for message sizes <=512 kiB, perf on ZionEX is better or equal with A2A-based all-gather rather than NCCL's default ring implementation -- the increased network congestion from A2A is more than made up for in the drop from O(N) to O(1) latency:
    {F974106345}
    
    See note for more details: https://fb.workplace.com/notes/1288352685369224
    
    Reviewed By: kingchc, minsii
    
    Differential Revision: D44903042
    
    fbshipit-source-id: 5201741da89e38b6d5a91e07ba3d8762ede19459

--- src/collectives/all_gather.cc
+++ src/collectives/all_gather.cc
@@ -7,10 +7,29 @@
 #include "enqueue.h"
 #include "collectives.h"
 
+NCCL_PARAM(AgDirectCutoff, "AG_DIRECT_CUTOFF", 512 * 1024);
+
 NCCL_API(ncclResult_t, ncclAllGather, const void* sendbuff, void* recvbuff, size_t sendcount,
     ncclDataType_t datatype, ncclComm_t comm, cudaStream_t stream);
 ncclResult_t ncclAllGather(const void* sendbuff, void* recvbuff, size_t sendcount,
     ncclDataType_t datatype, ncclComm_t comm, cudaStream_t stream) {
+  int nRanks = comm->nRanks;
+  size_t rankOffset = sendcount * ncclTypeSize(datatype);
+  bool directSend = (comm->localRanks == 1) && (rankOffset <= ncclParamAgDirectCutoff());
+
+  if (directSend) {
+    if (sendcount == 0) return ncclSuccess;
+
+    NCCLCHECK(ncclGroupStart());
+    for (int r = 0; r < nRanks; r++) {
+      NCCLCHECK(ncclSend(
+         ((char*)sendbuff), sendcount, datatype, r, comm, stream));
+      NCCLCHECK(ncclRecv(
+         ((char*)recvbuff) + r * rankOffset, sendcount, datatype, r, comm, stream));
+    }
+    NCCLCHECK(ncclGroupEnd());
+    return ncclSuccess;
+  }
   // Just pass the size of one message and not the total bytes sent/received.
   constexpr nvtxPayloadSchemaEntry_t AllGatherSchema[] = {
     {0, NVTX_PAYLOAD_ENTRY_TYPE_SIZE, "Message size [bytes]"}

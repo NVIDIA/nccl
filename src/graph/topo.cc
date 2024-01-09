41eb544a23f5035c526502dfbadfc66df65d3609
Peter Hawkins <phawkins@google.com>
2024-01-09 19:40:19 +0000

    Fix a handful of memory leaks in NCCL error paths.
    There are many cases in which NCCL fails to arrange that memory is freed
    if an error occurs. This PR fixes a couple I noticed occurring in practice.

--- src/graph/topo.cc
+++ src/graph/topo.cc
@@ -629,6 +629,7 @@ static ncclResult_t xmlInitAttrFloat(struct ncclXmlNode* node, const char* attrN
 ncclResult_t ncclTopoGetSystem(struct ncclComm* comm, struct ncclTopoSystem** system) {
   struct ncclXml* xml;
   NCCLCHECK(ncclCalloc(&xml, 1));
+  std::unique_ptr<void, void(*)(void*)> xml_ptr(xml, free);
   const char* xmlTopoFile = ncclGetEnv("NCCL_TOPO_FILE");
   if (xmlTopoFile) {
     INFO(NCCL_ENV, "NCCL_TOPO_FILE set by environment to %s", xmlTopoFile);
@@ -709,7 +710,6 @@ ncclResult_t ncclTopoGetSystem(struct ncclComm* comm, struct ncclTopoSystem** sy
   }
 
   NCCLCHECK(ncclTopoGetSystemFromXml(xml, system));
-  free(xml);
   return ncclSuccess;
 }
 

376a97e057fcc83303bfc978056981ff83532b6d
zhangjunjie <843707541@qq.com>
2026-05-18 13:39:28 +0800

    build: add net_observ plugin build support
    1. create net_observ plugin directory and its CMakeLists.txt
    2. add net_observ subdirectory to plugin build
    3. include net_observ source files in plugin build list
    4. fix Makefile include path and add net_observ source wildcard

---
+++ src/include/plugin/net_observ/net_observ.h
@@ -0,0 +1,11 @@
+/*************************************************************************
+ * SPDX-FileCopyrightText: Copyright (c) 2022-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * See LICENSE.txt for more license information
+ *************************************************************************/
+
+#ifndef NCCL_NET_OBSERV_H_
+#define NCCL_NET_OBSERV_H_
+
+#endif // NCCL_NET_OBSERV_H_
---
+++ src/plugin/net_observ/net_observ.cc
@@ -0,0 +1,8 @@
+/*************************************************************************
+ * SPDX-FileCopyrightText: Copyright (c) 2022-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * See LICENSE.txt for more license information
+ *************************************************************************/
+
+#include "net_observ/net_observ.h"

7ed469c7d6208d60f7a3b800acca59f1b57d547b
Lucas Wong <lucaswongchn@gmail.com>
2026-06-25 22:34:03 +0800

    Avoid RMA proxy when cuMem host is unavailable
    Guard CPU-accessible allocations with GDRCopy or cuMem host support.
    
    Signed-off-by: Lucas Wong <lucaswongchn@gmail.com>

--- src/init.cc
+++ src/init.cc
@@ -1669,8 +1669,12 @@ static ncclResult_t initTransportsRank(struct ncclComm* comm, struct ncclComm* p
   if (globalGinSupport && globalCuMemGdrSupport && !comm->hasMloPart) {
     comm->globalGinSupport = globalCrossNicSupport ? NCCL_GIN_CONNECTION_FULL : NCCL_GIN_CONNECTION_RAIL;
   }
-  comm->globalRmaProxySupport = globalRmaPluginSupport && globalCrossNicSupport && globalCuMemGdrSupport;
   isOneLsaTeams = ncclDevrIsOneLsaTeam(comm);
+  comm->globalRmaProxySupport = globalRmaPluginSupport && globalCrossNicSupport && globalCuMemGdrSupport;
+  if (!isOneLsaTeams && comm->globalRmaProxySupport && !ncclCpuAccessibleMemSupported()) {
+    INFO(NCCL_INIT, "RMA proxy is not supported because neither GDRCopy nor cuMem host allocations are available");
+    comm->globalRmaProxySupport = false;
+  }
   comm->symmetricSupport = comm->isAllCudaP2p && ncclParamWinEnable() && ncclCuMemEnable() &&
                            (comm->globalGinSupport != NCCL_GIN_CONNECTION_NONE || isOneLsaTeams);
   comm->hostRmaSupport =

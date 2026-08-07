86755accc65bb79ebba568c30a00de59f13151da
Wahid Mainassara <amainassarac@nvidia.com>
2026-08-07 22:38:47 +0200

    tuning: restore NVLSTree costs on two-node ARM systems
    Signed-off-by: Wahid Mainassara <amainassarac@nvidia.com>
    
    Mirrored-from: ac03dc35b45a3b46dff97cdf838ce605e53e3807

--- src/tuning/nvls.cc
+++ src/tuning/nvls.cc
@@ -161,7 +161,7 @@ ncclResult_t ncclTuningNvlsModelSim(struct ncclTuningInput_t* const inputs, stru
   if (nvlsTreeCorrection && (inputs->comm->cpuArch == NCCL_TOPO_CPU_ARCH_X86 ||
                              (inputs->comm->cpuArch == NCCL_TOPO_CPU_ARCH_ARM && inputs->comm->minNetBw >= 96.0f &&
                               inputs->comm->minLocalRanks == 4 && inputs->comm->maxLocalRanks == 4 &&
-                              (logSize < 23 || inputs->comm->nNodes > 2))))
+                              (logSize < 20 || inputs->comm->nNodes > 2))))
     bw *= treeCorrectionFactor[tuning->proto][logSize];
   tuning->timeUs = ncclTuningGetTime(inputs, tuning->algo, &lat, &bw);
   return ret;
